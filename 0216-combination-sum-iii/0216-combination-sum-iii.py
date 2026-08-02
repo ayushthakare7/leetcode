00de07abd655ff6a479e72df5becb63ec0ebaf4b
ayushthakare7 <educationf024@GMAIL.COM>
2026-08-02 12:26:46 +0530

    Time: 0 ms (100%), Space: 19.4 MB (33.06%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,29 +1,19 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        result = []
-        nums = [1,2,3,4,5,6,7,8,9]
+        ans = []
 
-        def solve(subset, index, total, k):
-            # Found a valid combination
-            if k == 0 and total == n:
-                result.append(subset.copy())
+        def backtrack(start, path, total):
+            if len(path) == k:
+                if total == n:
+                    ans.append(path[:])
                 return
 
-            # Invalid state
-            if index >= len(nums):
-                return
-            if k < 0:
-                return
-            if total > n:
-                return
+            for num in range(start, 10):
+                if total + num > n:
+                    break
+                path.append(num)
+                backtrack(num + 1, path, total + num)
+                path.pop()
 
-            # Include current number
-            subset.append(nums[index])
-            solve(subset, index + 1, total + nums[index], k - 1)
-            subset.pop()
-
-            # Exclude current number
-            solve(subset, index + 1, total, k)
-
-        solve([], 0, 0, k)
-        return result
+        backtrack(1, [], 0)
+        return ans
